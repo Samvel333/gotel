510d6f28c873065fc3940ca2a650fbae7d710e79
Samvel Sadoyan <s.sadoyan@digitalpome.com>
2022-08-28 17:31:19 +0400

    init

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
 
-func main()  {
-	fmt.Println("Hello World")
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func d() {
+	os.Setenv("BOT_TOKEN", "5223747597:AAEr-j4oJNMLauEVvpDDUqX_BTfzgNrEWnU")
+ }
+ 
+func main() {
+	d()
+    bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+    if err != nil {
+		fmt.Println("error unic co")
+        log.Panic(err)
+    }
+
+    bot.Debug = true
+
+    log.Printf("Authorized on account %s", bot.Self.UserName)
+
+    u := tgbotapi.NewUpdate(0)
+    u.Timeout = 60
+
+    updates := bot.GetUpdatesChan(u)
+
+    for update := range updates {
+        if update.Message == nil { // ignore any non-Message updates
+            continue
+        }
+
+        if !update.Message.IsCommand() { // ignore any non-command Messages
+            continue
+        }
+
+        // Create a new MessageConfig. We don't have text yet,
+        // so we leave it empty.
+        msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+        // Extract the command from the Message.
+        switch update.Message.Command() {
+        case "help":
+            msg.Text = "I understand /sayhi and /status."
+        case "sayhi":
+            msg.Text = "Hi :)"
+        case "status":
+            msg.Text = "I'm ok."
+        default:
+            msg.Text = "I don't know that command"
+        }
+
+        if _, err := bot.Send(msg); err != nil {
+            log.Panic(err)
+        }
+    }
 }
